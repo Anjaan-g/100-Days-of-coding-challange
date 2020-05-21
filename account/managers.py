96a688e0ab1df90bbcc83c71bc69e11c581f22e8
Anjaan-g <anjaangaire@gmail.com>
2020-05-21 17:01:58 +0545

    Initial commit (Day 1)

---
+++ account/managers.py
@@ -0,0 +1,45 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import ugettext_lazy as _
+
+class CustomUserManager(BaseUserManager):
+    def create_user(
+            self, email, first_name, last_name, password=None,
+            commit=True):
+        """
+        Creates and saves a User with the given email, first name, last name
+        and password.
+        """
+        if not email:
+            raise ValueError(_('Users must have an email address'))
+        if not first_name:
+            raise ValueError(_('Users must have a first name'))
+        if not last_name:
+            raise ValueError(_('Users must have a last name'))
+
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+        )
+
+        user.set_password(password)
+        if commit:
+            user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, first_name, last_name, password):
+        """
+        Creates and saves a superuser with the given email, first name,
+        last name and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
+            commit=False,
+        )
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
---
+++ account/models.py
@@ -0,0 +1,70 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.core.mail import send_mail
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.utils.translation import ugettext_lazy as _
+from django.utils import timezone
+from .managers import CustomUserManager
+
+# Create your models here.
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        verbose_name=_('email address'), max_length=255, unique=True
+    )
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+
+    is_admin = models.BooleanField(default=False)
+    is_retailer = models.BooleanField('retailer',default=False)
+
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_(
+            'Designates whether the user can log into this admin site.'
+        ),
+    )
+    # is_superuser field provided by PermissionsMixin
+    # groups field provided by PermissionsMixin
+    # user_permissions field provided by PermissionsMixin
+
+    date_joined = models.DateTimeField(
+        _('date joined'), default=timezone.now
+    )
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    class Meta:
+        verbose_name = _('Users')
+        verbose_name_plural = _('Users')
+
+    def get_full_name(self):
+        full_name = '%s %s'%(self.first_name,self.last_name)
+        return full_name.strip()
+    
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        send_mail(subject, message, from_email,[self.email], **kwargs)
+    
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
