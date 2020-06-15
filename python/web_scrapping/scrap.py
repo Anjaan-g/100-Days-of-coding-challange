5b35a5356c89a5f7a9b505e60b355e79dbc5644e
Anjaan-g <anjaangaire@gmail.com>
2020-06-15 23:13:39 +0545

    successfully scraped web data and written to a CSV file

--- python/web_scrapping/scrap.py
+++ python/web_scrapping/scrap.py
@@ -10,5 +10,30 @@ uClient.close()
 page_soup = soup(page_html, "html.parser")
 #finding the requested tag in the html file 
 containers = page_soup.findAll("div", {"class":"item-container"})
+
+filename = 'products.csv'
+f = open(filename, 'w')
+
+headers = "brand, product_name, shipping \n"
+
+f.write(headers)
+
+
+
 for container in containers:
-    brand = container.a.img["title"]
+    brand_container = container.find("div", {"class":"item-branding"})
+    brand = brand_container.a.img["title"]
+    
+    title_container= container.find("a", {"class":"item-title"})
+    product_name = title_container.text
+    
+    shipping_container = container.find("li", {"class":"price-ship"})
+    product_shipping = shipping_container.text.strip()
+
+    print("Brand: " + brand)
+    print("Product Name: " + product_name)
+    print("Shipping cost: " + product_shipping)
+
+    f.write(brand + "," + product_name.replace(",","|") + "," + product_shipping + "\n")
+
+f.close()
